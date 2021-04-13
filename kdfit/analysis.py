53ec7bb7306216b36bf0221a77713972b67f4bf0
Benjamin Land <benland100@gmail.com>
2021-04-13 15:25:56 -0400

    Add confidence interval calculation

--- kdfit/analysis.py
+++ kdfit/analysis.py
@@ -3,6 +3,7 @@ from .calculate import System
 from .term import Parameter,Sum
 
 import scipy.optimize as opt
+from functools import partial
     
 class Analysis:
     def __init__(self):
@@ -43,6 +44,11 @@ class Analysis:
             obs.load_data(fnames)
             
     def create_likelihood(self,verbose=False):
+        '''
+        Builds a calculation system that computes the sum of the log likelihood
+        from all Observables in this Analysis. This *must* be called again if
+        constant settings change for any Parameters, e.g. when profiling.
+        '''
         self._params = [p for p in self.parameters.values()]
         self._floated = [p for p in self._params if not p.constant]
         if verbose:
@@ -56,14 +62,93 @@ class Analysis:
             print('Ouput Values:',self._outputs)
         self._system = System(self._floated,self._outputs,verbose=verbose)
         
-    def __call__(self,min_params):
-        outputs = self._system.calculate(min_params)
+    def __call__(self,floated_params=None):
+        '''
+        Performs the current log likelihood calculation and returns the result.
+        
+        floated_params should be None to use parameter values, or values for all
+        floated parameters can be given in the order of self._floated_params to,
+        for example, evaluate the likelihood during minimization.
+        '''
+        if floated_params is None:
+            floated_params = [g if (g:=p.value) else 1.0 for p in self._floated]
+        outputs = self._system.calculate(floated_params)
         return outputs[0]
         
-    def minimize(self,fixed=(),**kwargs):
+    def minimize(self,**kwargs):
+        '''
+        Will optimize the floated parameters in the current log likelihood 
+        calculation to minimize the log likelihood.
+        
+        Keyword arguments are passed to scipy.optimize.minimize
+        '''
         initial = [g if (g:=p.value) else 1.0 for p in self._floated]
         minimum = opt.minimize(self,x0=initial,**kwargs)
         minimum.params = {p:v for p,v in zip(self._floated,minimum.x)}
+        return minimum
+        
+    def _delta_nll_profile(self,m,p,x,ci_delta=0.5,margs={}):
+        p.value = x
+        return self.minimize(**margs).fun - m.fun - ci_delta
+
+    def _delta_nll_scan(self,m,p,x,ci_delta=0.5):
+        p.value = x
+        return self() - m.fun - ci_delta
+
+    def confidence_intervals(self,minimum,method='scan',ci_delta=0.5,params=None,margs={}):
+        '''
+        Will either scan or profile the current likelihood to find the positive 
+        and negative confidence intervals for minimized parameters about the 
+        minimum.
+        
+        If profiling, the margs dictionary is used for keyword arguments to the
+        minimize function.
+        
+        The desired confidence interval is given by ci_delta, which is a delta
+        in the negative log likelihood from the minimum (0.5 for one sigma, etc)
+        '''
+        params = minimum.params if params is None else {p:minimum.params[p] for p in params}
+        initial_state = [(p.value,p.constant) for p in params]
+        upper,lower = {},{}
+        try:
+            # set all parameters to minimum values
+            for p,v in params.items():
+                p.value = v
+                p.constant = False
+            # compute confidence intervals for each parameter
+            for p,v in params.items():
+                p.constant = True
+                self.create_likelihood()
+                if method == 'profile':
+                    dnll = partial(self._delta_nll,minimum,p,margs=margs,ci_delta=ci_delta)
+                elif method == 'scan':
+                    dnll = partial(self._delta_nll_scan,minimum,p,ci_delta=ci_delta)
+                else:
+                    raise Exception('Unknown confidence interval method')
+                # brentq needs a bracketed interval. First try stepping half the
+                # central value left and right of the minimum. Then increase 
+                # step by a factor of two until the step is large enough to
+                # contain the desired root.
+                for step_factor in [0.1,0.2,0.5,1.0,2.0,5.0,10.0]:
+                    try:
+                        step = v*step_factor
+                        lo = opt.brentq(dnll,v-step,v,xtol=0.01,rtol=0.00001)
+                        hi = opt.brentq(dnll,v,v+step,xtol=0.01,rtol=0.00001)
+                        break
+                    except ValueError:
+                        step_factor *= 2
+                upper[p] = hi-v
+                lower[p] = v-lo
+                p.constant = False
+                p.value = v
+        finally:
+            # Put parameter settings back to how they were before
+            for p,(v,c) in zip(params,initial_state):
+                p.value = v
+                p.constant = c
+            self.create_likelihood()
+        minimum.upper = upper
+        minimum.lower = lower
         return minimum
         
 
